caa13c936f89dca7814713dff1682cc93069e2ae
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2023-05-13 16:50:12 +0200

    classes and freight states preparation

--- main.py
+++ main.py
@@ -1,6 +1,108 @@
+import datetime
 import logging
+from dataclasses import dataclass
+from enum import Flag
+from enum import auto
 
 from config import setup_logging
+
+
+class FreightState(Flag):
+    ACCEPTED = auto()
+    LOADING = auto()
+    LOADED = auto()
+    DRIVING = auto()
+    ARRIVED = auto()
+    UNLOADING = auto()
+    UNLOADED = auto()
+    FINISHING = auto()
+
+    TRANSITION = (
+            LOADING
+            | DRIVING
+            | UNLOADING
+            | FINISHING
+    )
+
+
+@dataclass
+class Freight:
+    _id: int
+    state: FreightState = FreightState.ACCEPTED
+
+    next_state_url = {
+        FreightState.ACCEPTED: 'freight_loading',
+        FreightState.LOADED  : 'freight_driving',
+        FreightState.ARRIVED : 'freight_unloading',
+        FreightState.UNLOADED: 'freight_finish',
+    }
+
+    def __post_init__(self):
+        self.state_gen = self.state_url_generator()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.state == FreightState.FINISHING:
+            raise StopIteration
+
+    def request_next_state(self, url):
+        pass
+
+    @staticmethod
+    def state_url_generator():
+        for state in FreightState:
+            if state not in FreightState.TRANSITION:
+                yield state
+
+
+@dataclass
+class Employee:
+    name: str
+    hired: datetime.date
+    salary: int
+    age: int
+    location: str
+    freight: Freight
+    awake: float
+
+
+class DrivingLicense(Flag):
+    DEFAULT = auto()
+    CONTAINER = auto()
+    TIPPER = auto()
+    COOLED = auto()
+    LIQUID = auto()
+    LOW = auto()
+    CHEMICAL = auto()
+    RADIOACTIVE = auto()
+    HEAVY = auto()
+
+
+class Trucker(Employee):
+    driving_license: DrivingLicense
+    damage_risk: float
+
+
+class WarehouseMan(Employee):
+    pass
+
+
+class Accountant(Employee):
+    pass
+
+
+class HRManager(Employee):
+    pass
+
+
+class Manager(Employee):
+    pass
+
+
+class TechnicalManager(Employee):
+    pass
 
 
 def main() -> int:
@@ -8,6 +110,8 @@ def main() -> int:
     log = logging.getLogger(__name__)
 
     log.info(' START '.center(80, '='))
+
+    f = Freight(123456)
 
     return 0
 
